60a98ddb991b9f6fff01b1c9bc6371978d0d3336
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-11-02 09:41:34 -0300

    fix

--- build/src/expect-call.js
+++ build/src/expect-call.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const mocha_1 = require("mocha");
+const chai_1 = require("chai");
 const chai = require("chai");
 const sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 function expectCall(stub, ...parameters) {
-    mocha_1.expect(stub).callCount(parameters.length);
-    mocha_1.expect(stub.args).to.be.eql(parameters);
+    chai_1.expect(stub).callCount(parameters.length);
+    chai_1.expect(stub.args).to.be.eql(parameters);
 }
 exports.expectCall = expectCall;
 //# sourceMappingURL=expect-call.js.map
--- src/expect-call.ts
+++ src/expect-call.ts
@@ -1,10 +1,11 @@
-import { expect } from 'mocha';
+import { expect as expectMocha } from 'mocha';
+import { expect } from 'chai'
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
 export function expectCall(stub: sinon.SinonStub | any, ...parameters: any[][]) {
-    expect(stub).callCount(parameters.length);
+    (expect(stub) as expectMocha).callCount(parameters.length);
     expect(stub.args).to.be.eql(parameters);
 }
