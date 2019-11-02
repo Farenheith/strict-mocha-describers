7bfeff6dc3b0f66a4e9e74a85b34f1baf223a876
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-11-02 09:31:27 -0300

    expectCall

---
+++ src/expect-call.ts
@@ -0,0 +1,10 @@
+import { expect } from 'mocha';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+export function expectCall(stub: sinon.SinonStub, parameters: any[][]) {
+    expect(stub).callCount(parameters.length);
+    expect(stub.args).to.be.eql(parameters);
+}
