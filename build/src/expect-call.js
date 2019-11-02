89e44a2d70f154cc445e7ce882ea4f8b7aa126ce
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-11-02 09:56:03 -0300

    better message

--- build/src/expect-call.js
+++ build/src/expect-call.js
@@ -9,7 +9,7 @@ function expectCall(stub, ...parameters) {
         .eql(parameters.length, `Expected ${stub.name} to have been called ${parameters.length} times but it was called ${stub.callCount} times.`);
     for (let i = 0; i < parameters.length; i++) {
         chai_1.expect(stub.args[i])
-            .eql(parameters[i], `Expected ${stub.name} to have been called with [${parameters[i]}] but it was called with [${stub.args[i]}]`);
+            .eql(parameters[i], `Expected call #${i} of ${stub.name} to have been called with [${parameters[i]}] but it was called with [${stub.args[i]}]`);
     }
 }
 exports.expectCall = expectCall;
