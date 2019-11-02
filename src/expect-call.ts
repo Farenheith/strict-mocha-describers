ff8bf9146bed70d978363efe9390bd7017eec0a8
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2019-11-02 09:51:53 -0300

    better message

--- src/expect-call.ts
+++ src/expect-call.ts
@@ -5,6 +5,20 @@ import * as sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
 export function expectCall(stub: sinon.SinonStub | any, ...parameters: any[][]) {
-    expect((stub as sinon.SinonStub).callCount).eql(parameters.length);
-    expect((stub as sinon.SinonStub).args).eql(parameters);
+    expect((stub as sinon.SinonStub).callCount)
+        .eql(parameters.length, `Expected ${
+            (stub as sinon.SinonStub).name
+        } to have been called ${parameters.length} times but it was called ${
+            (stub as sinon.SinonStub).callCount
+        } times.`);
+    for (let i = 0; i < parameters.length; i++) {
+        expect((stub as sinon.SinonStub).args[i])
+            .eql(parameters[i], `Expected ${
+                (stub as sinon.SinonStub).name
+            } to have been called with [${
+                parameters[i]
+            }] but it was called with [${
+                (stub as sinon.SinonStub).args[i]
+            }]`);
+    }
 }
